77bb76d16b1b0f40dde500673dc673d36dec85da
MuhammadUsamaAwan <underusama@gmail.com>
2025-08-11 15:38:44 +0500

    chore: init project

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import tsConfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [tsConfigPaths(), tailwindcss(), react()],
+});
